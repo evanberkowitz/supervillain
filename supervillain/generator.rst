d39acfc9890577d24e80ac6a993eb4a99919f8a9
Evan Berkowitz <git@evanberkowitz.com>
2023-08-25 12:08:31 -0400

    Fast implementation of NeighborhoodUpdate

--- supervillain/generator.rst
+++ supervillain/generator.rst
@@ -24,13 +24,16 @@ the result will be an ensemble ``e`` that has 17 identical configurations (becau
 Obviously, for good sampling, DoNothing is the worst imaginable algorithm!
 It has an infinitely bad ergodicity problem!
 
-A less dumb algorithm is a local update
+A less dumb algorithm is a local update,
+
+.. autoclass :: supervillain.generator.metropolis.SlowNeighborhoodUpdate
+   :members:
+
+The :class:`SlowNeighborhoodUpdate <supervillain.generator.metropolis.SlowNeighborhoodUpdate>` generator suffers from a variety of defects.
+
+First, its *implementation* makes a lot of calls.  We could 'unfactor' the proposal, single-site accept/reject, and sweep of the lattice for some benefits in speed.  Moreover, to compute the change in action it evaluates the action for both configurations and takes the difference.  But, we know that's silly, most of the $\phi$s and $n$s haven't changed at all and those links will contribute to both actions equally, giving zero difference.  We could reduce the arithmetic substantially by computing the difference directly.
 
 .. autoclass :: supervillain.generator.NeighborhoodUpdate
    :members:
-
-This class suffers from a variety of defects.
-
-First, its *implementation* makes a lot of calls.  We could 'unfactor' the proposal, single-site accept/reject, and sweep of the lattice for some benefits in speed.  Moreover, to compute the change in action it evaluates the action for both configurations and takes the difference.  But, we know that's silly, most of the $\phi$s and $n$s haven't changed at all and those links will contribute to both actions equally, giving zero difference.  We could reduce the arithmetic substantially by computing the difference directly.
 
 But, also, as an *algorithm* the neighborhood update suffers because it can only make small changes in a local area.  Smarter algorithms can make high-acceptance updates to many variables across the lattice, which can help overcome *critical slowing down*.
--- supervillain/generator/metropolis.py
+++ supervillain/generator/metropolis.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
 
 import numpy as np
+import supervillain.action
 
 import logging
 logger = logging.getLogger(__name__)
 
-class NeighborhoodUpdate:
+class SlowNeighborhoodUpdate:
     r'''
     A neighborhood update changes only fields in some small area of the lattice.
 
@@ -22,9 +23,21 @@ class NeighborhoodUpdate:
         \end{align}
 
     for the $\phi$ on $x$ and $n$ on links $\ell$ which touch $x$.
+
+    Parameters
+    ----------
+    action: Villain
+        The action from which we sample.
+    interval_phi: float
+        A single float used to construct the uniform distribution for $\phi$.
+    interval_n: int
+        A single integer that gives the biggest allowed changes to $n$.
     '''
 
     def __init__(self, action, interval_phi=np.pi, interval_n=1):
+
+        if not isinstance(action, supervillain.action.Villain):
+            raise ValueError('The Neighborhood Metropolis update requires the Villain action.')
         self.Action       = action
         self.interval_phi = interval_phi
         self.interval_n   = interval_n
@@ -155,3 +168,119 @@ class NeighborhoodUpdate:
             +'\n'+
             f'    {self.acceptance / self.sweeps:.6f} average Metropolis acceptance probability.'
         )
+
+
+class NeighborhoodUpdate:
+    r'''
+    This performs the same update as :class:`SlowNeighborhoodUpdate <supervillain.generator.metropolis.SlowNeighborhoodUpdate>` but is streamlined to eliminate calls and to calculate the change in action directly.
+
+    .. note ::
+       On a small 5×5 example this generator yields about three times as many updates per second than :class:`SlowNeighborhoodUpdate <supervillain.generator.metropolis.SlowNeighborhoodUpdate>` on my machine!
+       This ratio should *improve* for larger lattices because the change in action is computed directly and is of fixed cost, rather than scaling with the volume.
+    '''
+
+    def __init__(self, action, interval_phi=np.pi, interval_n=1):
+        if not isinstance(action, supervillain.action.Villain):
+            raise ValueError('The Neighborhood Metropolis update requires the Villain action.')
+        self.Action       = action
+        self.Lattice      = action.Lattice
+        self.kappa        = action.kappa
+
+        self.interval_phi = interval_phi
+        self.interval_n   = interval_n
+
+        self.rng = np.random.default_rng()
+        self.n_changes = np.arange(-interval_n, 1+interval_n)
+
+        self.accepted = 0
+        self.proposed = 0
+        self.acceptance = 0.
+        self.sweeps = 0
+
+    def step(self, cfg):
+        r'''
+        Make volume's worth of random single-site updates.
+
+        Parameters
+        ----------
+        cfg: dict
+            A dictionary with phi and n field variables.
+
+        Returns
+        -------
+        dict
+            Another configuration of fields.
+        '''
+
+        self.sweeps += 1
+        total_acceptance = 0
+        accepted = 0
+
+        phi = cfg['phi'].copy()
+        n   = cfg['n'].copy()
+
+        # Rather than sweeping the lattice in a particular order, we randomly update sites.
+        shifts = np.stack((
+            np.random.randint(self.Lattice.dims[0], size=self.Lattice.sites),
+            np.random.randint(self.Lattice.dims[1], size=self.Lattice.sites)
+        )).transpose()
+
+        for dx in shifts:
+            # We move the lattice around (which is fine by translational symmetry)
+            # so that we update a different site with each proposal.
+            # The advantage of thinking this way is that we only have to reckon from the origin.
+            phi =  self.Lattice.roll(phi, dx)
+            n   =  self.Lattice.roll(n,   dx)
+
+            change_phi = self.rng.uniform(-self.interval_phi,+self.interval_phi,None)
+            change_n = self.rng.choice(self.n_changes,4)
+
+            # We don't even construct a new field until we know whether we know we'll accept or reject.
+            # We can calculate dS directly from just the previous values and the proposed changes.
+            # This formula is the application of the difference of two squares for each changed link.
+            dS = 0.5*self.kappa*(
+                +(-change_phi-2*np.pi*change_n[0])*(2*(phi[+1,+0]-phi[+0,+0]-2*np.pi*n[0][+0,+0])-change_phi-2*np.pi*change_n[0])
+                +(+change_phi-2*np.pi*change_n[1])*(2*(phi[+0,+0]-phi[-1,+0]-2*np.pi*n[0][-1,+0])+change_phi-2*np.pi*change_n[1])
+                +(-change_phi-2*np.pi*change_n[2])*(2*(phi[+0,+1]-phi[+0,+0]-2*np.pi*n[1][+0,+0])-change_phi-2*np.pi*change_n[2])
+                +(+change_phi-2*np.pi*change_n[3])*(2*(phi[+0,+0]-phi[+0,-1]-2*np.pi*n[1][+0,-1])+change_phi-2*np.pi*change_n[3])
+            )
+
+            # Now we Metropolize
+            acceptance = np.clip( np.exp(-dS), a_min=0, a_max=1)
+            total_acceptance += acceptance
+            metropolis = self.rng.uniform(0,1,None)
+            if metropolis < acceptance:
+                logger.debug(f'Proposal accepted; ∆S = {dS:f}; acceptance probability = {acceptance:f}')
+                accepted += 1
+                # and conditionally update the configuration.
+                phi[0,0] += change_phi
+                # These assignments are picked to match the unrolled dS calculation.
+                n[0][+0,+0] += change_n[0]
+                n[0][-1,+0] += change_n[1]
+                n[1][+0,+0] += change_n[2]
+                n[1][+0,-1] += change_n[3]
+
+            else:
+                logger.debug(f'Proposal rejected; ∆S = {dS:f}; acceptance probability = {acceptance:f}')
+
+        self.accepted += accepted
+        self.proposed += len(shifts)
+
+        total_acceptance /= len(shifts)
+        self.acceptance += total_acceptance
+        logger.info(f'Average proposal {acceptance=:.6f}; Actually {accepted = } / {self.Action.Lattice.sites} = {accepted / self.Action.Lattice.sites}')
+
+        return {'phi': phi, 'n': n}
+
+    def report(self):
+        r'''
+        Returns a string with some summarizing statistics.
+        '''
+        return (
+            f'There were {self.accepted} single-site proposals accepted of {self.proposed} proposed updates.'
+            +'\n'+
+            f'    {self.accepted/self.proposed:.6f} acceptance rate' 
+            +'\n'+
+            f'    {self.acceptance / self.sweeps:.6f} average Metropolis acceptance probability.'
+        )
+
