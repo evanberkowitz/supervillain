13b1a1fa010f685f2bf73f0e42b9f8f3fe90bf13
Evan Berkowitz <git@evanberkowitz.com>
2023-09-10 22:24:49 -0500

    Worldline Winding_Winding and WindingSquared
    * Derive the observable which matches the Villain observable.
    * Functional derivative of dδJ is the 2D 5-point stencil.
    * Documentation.

--- supervillain/observable/topology.py
+++ supervillain/observable/topology.py
@@ -34,6 +34,18 @@ class WindingSquared(Observable):
         L = S.Lattice
         return np.mean(L.d(1, n)**2)
 
+    @staticmethod
+    def Worldline(S, m):
+        r'''
+        Reusing the derivation from :class:`~.Winding_Winding` and setting $p=q$ we get
+
+        .. math::
+
+            w_p =  \frac{1}{\pi^2 \kappa}-\frac{1}{(2\pi \kappa)^2}\left\langle (dm)_p^2 \right\rangle.
+
+        because $\delta / \delta J_p ( d \delta J_p) = 4$.
+        '''
+        return 1/(np.pi**2 * S.kappa)-np.mean(S.Lattice.d(1, m)**2) / (2*np.pi*S.kappa)**2
 
 class Winding_Winding(Observable):
     r'''
@@ -61,4 +73,105 @@ class Winding_Winding(Observable):
         L = S.Lattice
         dn = L.d(1, n)
         return L.correlation(dn, dn)
+
+    @staticmethod
+    def Worldline(S, m):
+        r'''
+        .. collapse :: The Worldline observable is trickier.
+            :class: note:
+
+            Expanding the :class:`~.Worldline` action (and grouping the irrelevant contants)
+
+            .. math::
+                \begin{align}
+                S_J[m]
+                    &= \frac{1}{2\kappa} \sum_\ell \left(m - \frac{\delta J}{2\pi}\right)_\ell^2 + \text{constants}
+                    \\
+                    &= \frac{1}{2\kappa} \sum_\ell \left(m_\ell^2 - \frac{1}{\pi} m_\ell (\delta J)_\ell + \frac{1}{4\pi^2} (\delta J)_\ell^2 \right)+ \text{constants}
+                    \\
+                    &= \frac{1}{2\kappa} \left(\sum_\ell m_\ell^2 + \sum_p - \frac{1}{\pi} (dm)_p J_p + \frac{1}{4\pi^2} J_p (d \delta J)_p \right)+ \text{constants}
+                \end{align}
+
+            where we integrated two terms by parts to get $J$ undecorated.
+
+            Differentiating $\log Z$ once gives
+
+            .. math::
+                \begin{align}
+                \frac{\delta}{\delta J_p} \log Z
+                    &= \frac{1}{Z} \sum Dm\; [\delta m = 0] e^{-S_J[m]} \frac{\delta S}{\delta J_p}
+                    \\
+                    &= \frac{1}{Z} \sum Dm\; [\delta m = 0] e^{-S_J[m]} \frac{-1}{2\kappa}\left( -\frac{1}{\pi} (dm)_p + \frac{2}{4\pi^2} (d\delta J)_p \right)
+                \end{align}
+
+            where the factor of 2 on the $d \delta J$ term comes from the fact that $J d \delta J$ is quadratic in $J$.
+
+            Differentiating again we must hit both the $1/Z$ term and the path integral upstairs giving two terms,
+
+            .. math::
+                \begin{align}
+                \frac{\delta}{\delta J_q}\frac{\delta}{\delta J_p} \log Z
+                    =& \frac{\delta}{\delta J_q} \left[\frac{1}{Z} \sum Dm\; [\delta m = 0] e^{S_J[m]} \frac{-1}{2\kappa}\left( -\frac{1}{\pi} (dm)_p + \frac{1}{2\pi^2} (d\delta J)_p \right) \right]
+                    \\
+                    =& -\frac{1}{Z^2} \left[\sum Dm\; [\delta m = 0] e^{S_J[m]} \frac{-1}{2\kappa}\left( -\frac{1}{\pi} (dm)_p + \frac{1}{2\pi^2} (d\delta J)_p \right) \right]
+                    \\
+                    & \phantom{-\frac{1}{Z^2}}\times \left[\sum Dm\; [\delta m = 0] e^{S_J[m]} \frac{-1}{2\kappa}\left( -\frac{1}{\pi} (dm)_q + \frac{1}{2\pi^2} (d\delta J)_q \right) \right]
+                    \\
+                    &+ \frac{1}{Z} \sum Dm\; [\delta m = 0] e^{S_J[m]} \Bigg[
+                    \\
+                    & \phantom{-\frac{1}{Z^2}} \left(\frac{-1}{2\kappa}\right)^2\left( -\frac{1}{\pi} (dm)_q + \frac{1}{2\pi^2} (d\delta J)_q \right)\left( -\frac{1}{\pi} (dm)_p + \frac{1}{2\pi^2} (d\delta J)_p \right)
+                    \\
+                    & \phantom{-\frac{1}{Z^2}} +\frac{-1}{4\pi^2 \kappa}\frac{\delta}{\delta J_q} (d \delta J)_p 
+                    \Bigg]
+                \\
+                \end{align}
+
+            In the second term we differentiate both the action [the first term in the brackets] and what was already brought down [the second].
+            Plugging in $J=0$ gives
+
+            .. math ::
+                \begin{align}
+                -\left.\frac{\delta}{\delta J_q}\frac{\delta}{\delta J_p} \log Z \right|_{J=0}
+                = \left(\frac{1}{2\pi \kappa}\right)^2\Bigg\{&
+                \left\langle (dm)_p \right\rangle \left\langle (dm)_q \right\rangle
+                \\
+                &- \left\langle (dm)_p (dm)_q \right\rangle
+                \\
+                & + \kappa \left.\frac{\delta}{\delta J_q} (d \delta J)_p\right|_{J=0} \Bigg\}
+                \end{align}
+
+            because $d\delta 0 = 0$.
+
+            When $J=0$ the first (quantum-disconnected) term is proportional $\left\langle dm_p \right\rangle \left\langle dm_q \right\rangle$ and the individual expectation values vanish by symmetry so we are left with
+
+            .. math ::
+                \begin{align}
+                -\left.\frac{\delta}{\delta J_q}\frac{\delta}{\delta J_p} \log Z \right|_{J=0}
+                = \left(\frac{1}{2\pi \kappa}\right)^2\Bigg\{&
+                \kappa \left.\frac{\delta}{\delta J_q} (d \delta J)_p  \right|_{J=0}
+                - \left\langle (dm)_p (dm)_q \right\rangle\Bigg\}
+                \end{align}
+
+            The remaining functional derivative is a displacement-dependent constant.
+
+
+        At the end of the day all we are left with is
+
+        .. math::
+            - \frac{\delta}{\delta J_p} \frac{\delta}{\delta J_q}\log Z = \frac{1}{(2\pi \kappa)^2}\left\{\kappa\left.\frac{\delta}{\delta J_q}(d \delta J_p)\right|_{J=0} - \left\langle (dm)_p (dm_q) \right\rangle\right\}
+
+        when $J=0$.  In 2D $\left.\frac{\delta}{\delta J_q}(d \delta J_p)\right|_{J=0} = 4 \delta_{pq} - \sum_{\hat{\mu}} \delta_{p+\hat{\mu},q}$ where $\hat{\mu}$ runs over the 4 cardinal directions, reproducing (minus) the standard `2D five-point Laplacian stencil <https://en.wikipedia.org/wiki/Five-point_stencil#In_two_dimensions>`_.
+        '''
+        L = S.Lattice
+        kappa = S.kappa
+        dm = L.d(1, m)
+
+        # δ/δJ_p (d δ (J_q)) doesn't vanish, but neither does it depends on J.
+        # In fact, it is a constant that depends only the relative coordinate.
+        # We can get the stencil by computing δ d (all zeros except at the origin)
+        no_displacement = L.form(2)
+        no_displacement[0,0] = 1.
+        d_delta_J = L.d(1, L.delta(2, no_displacement))
+
+        return (kappa * d_delta_J - L.correlation(dm, dm)) / (2*np.pi*kappa)**2
 
