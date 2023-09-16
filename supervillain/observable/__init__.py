553670d632d3bda4eab41e808788457641827c84
Evan Berkowitz <git@evanberkowitz.com>
2023-09-15 20:02:07 -0600

    Move ActionDensity to new file.

--- supervillain/observable/__init__.py
+++ supervillain/observable/__init__.py
@@ -2,5 +2,5 @@ from .observable import Observable
 from .derived import DerivedQuantity
 
 from .energy import InternalEnergyDensity, InternalEnergyDensitySquared
-from .energy import ActionDensity
+from .action import ActionDensity
 from .topology import WindingSquared, Winding_Winding
