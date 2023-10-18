403308dfb2ed54bf7e65f9f300a306a7e5a0a6bd
Seth Buesing <coepsbuesing@gmail.com>
2023-10-18 05:18:13 -0500

    Added rudimentary "burrow" subroutine to worms.py. Requires lots of further testing, but seems to properly give a single burrow step.

--- supervillain/generator/__init__.py
+++ supervillain/generator/__init__.py
@@ -4,3 +4,4 @@ from .example import DoNothing
 from .metropolis import NeighborhoodUpdate
 import supervillain.generator.constraint
 import supervillain.generator.combining
+import supervillain.generator.worms
