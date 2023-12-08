01efb96c37920371885232bc6475bc2112fb88d3
Evan Berkowitz <git@evanberkowitz.com>
2023-12-08 16:42:02 +0100

    topology.py --> winding.py

--- supervillain/observable/__init__.py
+++ supervillain/observable/__init__.py
@@ -4,7 +4,7 @@ from .derived import DerivedQuantity
 from .links import Links
 from .energy import InternalEnergyDensity, InternalEnergyDensitySquared
 from .action import ActionDensity, ActionTwoPoint, Action_Action
-from .topology import WindingSquared, Winding_Winding
+from .winding import WindingSquared, Winding_Winding
 from .wrapping import TorusWrapping, TWrapping, XWrapping
 from .spin import Spin_Spin, SpinSusceptibility, SpinSusceptibilityScaled
 from .vortex import Vortex_Vortex, VortexSusceptibility, VortexSusceptibilityScaled
