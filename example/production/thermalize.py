429427dd26e65577a90db03c4a862df3578844e6
Evan Berkowitz <git@evanberkowitz.com>
2024-09-09 09:43:55 +0200

    --parallel-files for debugging

--- example/production/thermalize.py
+++ example/production/thermalize.py
@@ -23,10 +23,15 @@ if __name__ == '__main__':
     parser = supervillain.cli.ArgumentParser()
     parser.add_argument('input_file', type=supervillain.cli.input_file('input'), default='input.py')
     parser.add_argument('--parallel', default=False, action='store_true')
+    parser.add_argument('--parallel-files', default=False, action='store_true', help='Store not in the usual storage spots but instead where it would be stored in a --parallel computation.  Useful for testing / debugging.')
 
     args = parser.parse_args()
 
     if not args.parallel:
+        if args.parallel_files:
+            from parallel import io_prep
+            args.input_file.ensembles = args.input_file.ensembles.apply(io_prep, axis=1)
+
         from tqdm.autonotebook import tqdm
         from tqdm.contrib.logging import logging_redirect_tqdm
         import steps
